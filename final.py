a0d9814812558d1e52c5b1c113af5525fa861d56
xinzefan <xinzefan813@gmail.com>
2020-11-19 15:37:01 -0600

    add a little assumptions

---
+++ final.py
@@ -0,0 +1,4 @@
+# Assumption 1 : Is the area with more water coverage has a less change in the amount of ozone layer?
+#				 If it is, how much its changed in percentage less than other area?
+# Assumption 2 : Is the volcano's erruption influenced the ozone layer? if so, how much it was influcened?
+#
